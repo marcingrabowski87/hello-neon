fa0aa03328b7d7d3a8526e541c65e00cedf1faff
mgrabowski87 <mgrabowski87@gmail.com>
2019-02-27 07:42:39 +0100

    Usuniecje komentarzy z kodu

--- hamburger.js
+++ hamburger.js
@@ -1,35 +1,12 @@
-var widthScreen;
-var actualWidth;
-
 function action() {
     document.querySelector('nav').classList.toggle('active');
-
-    //return widthScreen = window.innerWidth;
-    //console.log(widthScreen);
 
 }
 
 function removeaction() {
     document.querySelector('nav').classList.remove('active');
 
-
 }
-
-
-//window.addEventListener('resize', callback);
-
-/*function callback() {
-    return actualWidth = window.innerWidth;
-}*/
-
-
-//console.log(action());
-/*if (action() < 1024 && document.querySelector('nav.active') == "active") {
-    document.querySelector('nav').classList.remove('active');
-
-}*/
-
-
 
 document.querySelector('.hamburger').addEventListener('click', action)
 document.querySelectorAll('nav ul li a').forEach(function (clickedLink) {
